98e58fdd638aa4f99b1e58663a0ff3760040324d
Alfredo Garcia <alfredd.gco@gmail.com>
2022-06-11 22:36:29 -0500

    First commit

---
+++ scripts/ois_trainval.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import os, random
+
+images = os.listdir('OIS/images')
+train_file = open('OIS/train.txt', 'w')
+valid_file = open('OIS/valid.txt', 'w')
+
+for image in images:
+  if(random.random() <= 0.2):
+    valid_file.write('%s\n' % image)
+  else:
+    train_file.write('%s\n' % image)
+
+train_file.close()
+valid_file.close()
