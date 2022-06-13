a730bf4f4908abfe1bf4e8aec2186ab26e727461
Alfredo Garcia <alfredd.gco@gmail.com>
2022-06-12 23:07:26 -0500

    Hotfix script

--- scripts/ois_trainval.py
+++ scripts/ois_trainval.py
@@ -7,9 +7,9 @@ valid_file = open('OIS/valid.txt', 'w')
 
 for image in images:
   if(random.random() <= 0.2):
-    valid_file.write('%s\n' % image)
+    valid_file.write('OIS/images/%s\n' % image)
   else:
-    train_file.write('%s\n' % image)
+    train_file.write('OIS/images/%s\n' % image)
 
 train_file.close()
 valid_file.close()
