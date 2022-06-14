5bfb64b4be537f0e68e743d051ae3c81902d65b0
Alfredo Garcia <alfredd.gco@gmail.com>
2022-06-14 16:41:23 -0500

    Optimizing video reading

--- python/video_detect.py
+++ python/video_detect.py
@@ -1,10 +1,9 @@
 from ctypes import *
-import math
-import random
-import time 
-import cv2
-import sys
+import sys, time
+import cv2, pydash
 import numpy as np
+from imutils.video import FPS, FileVideoStream
+
 
 def c_array(ctype, values):
     arr = (ctype*len(values))()
@@ -136,12 +135,20 @@ def detect(net, meta, im, thresh=.5, hier_thresh=.5, nms=.45):
                 b = dets[j].bbox
                 res.append((meta.names[i], dets[j].prob[i], (b.x, b.y, b.w, b.h)))
     res = sorted(res, key=lambda x: -x[1])
+    out = []
+    for detection in res:
+        t = {
+            'label': detection[0],
+            'confidence': detection[1],
+            'position': detection[2],
+        }
+        out.append(t)
     free_image(im)
     free_detections(dets, num)
-    return res
+    return out
 
 
-def opencv_img_to_darknet_img(img):
+def cv_img_to_darknet_img(img):
     h, w, c = img.shape
 
     img = img.transpose(2, 0, 1)[::-1]
@@ -154,26 +161,64 @@ def opencv_img_to_darknet_img(img):
     return darknet_img
 
 
+def probability_filter(detections):
+    detections = pydash.arrays.uniq_by(detections, lambda x: x['label'])
+    filtered = []
+    THRESHOLDS = {
+        'Stop': 0.5,
+        'No speed limit': 0.75,
+        'Turn right': 0.55,
+        'Ahead only': 0.8,
+    }
+    for detection in detections:
+        if(detection['confidence'] >= THRESHOLDS[detection['label'].decode()]):
+            filtered.append(detection)
+    return filtered
+
+
+def resize_img(frame):
+    h, w, c = frame.shape
+    print(frame.shape)
+    frame = frame[h//3:]
+    print(frame.shape)
+    fraction = w*2//5
+    fraction = (frame.shape[0] // 2)
+    frame = np.concatenate((frame[:,:fraction] + frame[:,-fraction:]), axis=1)
+    print(frame.shape)
+    frame = cv2.resize(frame, (416, 416))
+    return frame
+
+
 if __name__ == "__main__":
     net = load_net(b"cfg/ois.cfg", b"weights/ois_final.weights", 0)
     meta = load_meta(b"cfg/ois.data")
 
-    cap = cv2.VideoCapture('data/stop_video.mp4')
-    frames = 0
-    start = time.time()
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if(ret):
-            frames += 1
-            img = opencv_img_to_darknet_img(frame)
-            r = detect(net, meta, img)
-            if(frames % 30 == 0):
-                print(r)
-        else:
-            break
-
-    elapsed = time.time() - start
-    cap.release()    
-    print("Detection time: %.3f seconds. %d FPS" % (elapsed, frames // elapsed))
+    video_stream = None
+    if(len(sys.argv) > 1):
+        video_stream = FileVideoStream(sys.argv[1]).start()
+    else:
+        video_stream = FileVideoStream('data/stop_video.mp4').start()
+    time.sleep(1)
     
+    last_detected = None
+    timer = FPS().start()
+    while video_stream.more():
+        frame = video_stream.read()
+        if(frame is None):
+            break
+        # cv2.imshow('frame', frame)
+        # cv2.waitKey(1)
+        h, w, c = frame.shape
+        frame = resize_img(frame)
+        exit()
+        img = cv_img_to_darknet_img(frame)
+        r = detect(net, meta, img)
+        r = probability_filter(r)
+        if(len(r) > 0 and r[0]['label'] != last_detected):
+            print(pydash.collections.map_(r, 'label'), pydash.collections.map_(r, 'confidence'))
+            last_detected = r[0]['label']
+        timer.update()
+
+    timer.stop()
+    print("Detection time: {:.3f} seconds. {:.2f} FPS".format(timer.elapsed(), timer.fps()))
 
